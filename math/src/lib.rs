7c9ace662fb62325901b681bc99781683e0a4ad7
Cooper Johnson <lavosprime@gmail.com>
2023-05-21 20:44:25 -0700

    math: clean up lib.rs

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,15 +1,3 @@
+#![forbid(unsafe_code)]
+
 pub mod vector;
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
